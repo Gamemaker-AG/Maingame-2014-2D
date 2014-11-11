a9e53fbc59ae41a3152283cd52048a50977cdb9c
Saremox <saremox@linux.com>
2014-11-11 21:52:39 +0100

    Implement floating object script

---
+++ Assets/3strassb/Scripts/FloatingIsland.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloatingIsland : MonoBehaviour {
+	public Vector3 start, end;
+	public float time;
+	private Vector3 speed;
+	private Transform trans;
+	private Vector3 destination;
+
+	void Awake ()
+	{
+		trans = GetComponent<Transform>();
+		speed = new Vector3((end.x - start.x)/time,
+							(end.y - start.y)/time, 
+							(end.z - start.z)/time);
+		destination = end;
+	}
+
+	void Start () {
+		
+		trans.position = start;
+		
+	}
+	
+	void FixedUpdate () {
+		if(Vector3.Distance(trans.position,destination) > 0.1f)
+		{
+			trans.Translate(speed*Time.deltaTime);
+		}
+		else
+		{
+			speed = speed * -1;
+			if(destination == start)
+			{
+				destination = end;
+			}
+			else
+			{
+				destination = start;
+			}
+		}
+	}
+
+	void OnDrawGizmosSelected()
+    {
+    	Gizmos.color = new Color(1,0,0,1f);
+		Gizmos.DrawCube(start + new Vector3(0,0,-3), 	new Vector3(0.2f,0.2f,0.2f));
+		Gizmos.DrawCube(end   + new Vector3(0,0,-3), 	new Vector3(0.2f,0.2f,0.2f));
+		Gizmos.DrawLine(start,end);
+	}
+}
