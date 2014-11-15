b3e1c88a6ba31469ae9dee45325e8fb496805f4a
Saremox <saremox@linux.com>
2014-11-15 20:40:41 +0100

    Add Animation for player

---
+++ Assets/3strassb/Scripts/AnimationController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationController : MonoBehaviour {
+	private Animator _animation;
+	private Transform _trans;
+	private Vector3 _lastposition;
+	
+	public void Awake()
+	{
+		_trans = GetComponent<Transform>();
+		_animation = GetComponent<Animator>();
+		_lastposition = _trans.position;
+	}
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(_lastposition == _trans.position)
+		{
+			_animation.Play("Standing");
+		}
+		else 
+		{
+			_animation.Play("Leg");
+		}
+		_lastposition = _trans.position;
+	}
+}
