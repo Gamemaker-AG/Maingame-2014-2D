a29e691a0aa282206af946e80e5d39bcd41f424f
Saremox <saremox@linux.com>
2014-11-21 23:37:05 +0100

    Adds a basic GUI for points and implements point collection

--- Assets/3strassb/Scripts/Collectable.cs
+++ Assets/3strassb/Scripts/Collectable.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 [RequireComponent (typeof (Collider2D))]
 public class Collectable : MonoBehaviour {
+	private bool collected = false;
+
 
 	void OnTriggerEnter2D(Collider2D collision) 
 	{
-		Destroy(transform.gameObject);
+		if(!collected && collision.CompareTag("Player"))
+		{
+			collected = true;
+			Destroy(gameObject);
+			collision.SendMessage("AddPoints",1,SendMessageOptions.DontRequireReceiver);
+
+		}
     }
 }
---
+++ Assets/3strassb/Scripts/PlayerGUI.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerGUI : MonoBehaviour 
+{
+	private int points = 0;
+
+	void OnGUI()
+	{
+		GUI.Box(new Rect(10, 10, 160, 60), "Points");
+		GUI.Label(new Rect(15, 30, 140, 30), points.ToString());
+	}
+
+	void AddPoints(int p)
+	{
+		points += p;
+	}
+}
