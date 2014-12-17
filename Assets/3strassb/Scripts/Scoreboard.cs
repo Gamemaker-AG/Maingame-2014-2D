a37d4c059a043ecc69f0d85510cdb2a6ea6de278
Saremox <saremox@linux.com>
2014-12-17 21:11:19 +0100

    Fix null reference exceptions then gameobjects on a elevator get destroyed

---
+++ Assets/3strassb/Scripts/Scoreboard.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Scoreboard : MonoBehaviour {
+	public GameObject pointDisplay;
+	private Text myText;
+	private int curPoints;
+
+	public void Start () 
+	{
+		myText = pointDisplay.GetComponent<Text> ();
+	}
+
+	public void AddPoints(int points)
+	{
+		curPoints += points;
+	}
+
+	public void Update () 
+	{
+		myText.text = curPoints.ToString ();
+	}
+}
